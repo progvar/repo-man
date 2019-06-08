0a5ac3d834409b186b481d682b43ba82baefb7d6
Gyula Varadi <gyula.varadi@lhsystems.com>
2019-06-08 23:32:44 +0200

    feat(search): add basic search functionality

--- src/app/modules/search/search.component.ts
+++ src/app/modules/search/search.component.ts
@@ -1,14 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Repo, SearchResponse } from './search';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+    public searchQuery = '';
+    public repos: Subject<Repo[]> = new Subject();
 
-  constructor() { }
+    constructor(private httpClient: HttpClient) {}
 
-  ngOnInit() { }
+    onSearch(query: string): void {
+        if (!query) {
+            return;
+        }
+
+        const url = 'https://api.github.com/search/repositories';
+        const params: { [key: string]: string } = {
+            q: query
+        };
+
+        this.httpClient
+            .get<SearchResponse>(url, { params })
+            .subscribe((res: SearchResponse) => {
+                this.repos.next(res.items);
+            });
+    }
 
 }
--- src/app/modules/search/search.module.ts
+++ src/app/modules/search/search.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { SearchComponent } from './search.component';
 
 @NgModule({
   declarations: [SearchComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    HttpClientModule
   ],
   exports: [SearchComponent]
 })
